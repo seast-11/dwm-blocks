34ab56fabe0b006865cea0b7a017c9d041d31375
sakarsoy <sakarsoy@yahoo.com>
2024-05-06 20:11:52 -0400

    updated blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,14 @@
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
-	{"", 		"~/.local/bin/traffic",  					2,			19},
-	{"", 		"~/.local/bin/temperature",					5,			18},
-	{"", 		"~/.local/bin/utilization",					2,			21},
-	{"", 		"~/.local/bin/disk",						0,			15},
-	{"", 		"~/.local/bin/update",						0,			14},
-	{"", 		"~/.local/bin/memory",						5,			11},
-	{"", 		"~/.local/bin/clock",						60,			20},
-	{"", 		"~/.local/bin/volume",						0,			10},
-	{"", 		"~/.local/bin/system",						0,			17},
+	/*Icon*/	/*Command*/		                                              /*Update Interval*/	/*Update Signal*/
+	{"", 		"~/.local/bin/traffic",  					                            2,			            19},
+	{"", 		"~/.local/bin/temperature",					                          15,			            18},
+	{"", 		"~/.local/bin/utilization",					                          2,			            21},
+	{"", 		"~/.local/bin/disk",						                              300,		            15},
+	{"", 		"~/.local/bin/update",						                            0,			            14},
+	{"",    "~/.local/bin/memory",	                                      15,		              0},
+	{"",    "~/.local/bin/clock",	                                        15,		              0},
+	{"", 		"~/.local/bin/volume",						                            0,			            10},
+	{"", 		"~/.local/bin/connectivity.sh",		                            15,			            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
